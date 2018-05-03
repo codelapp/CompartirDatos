9032de079551525afe3cc770d02d7e67017ba629
codelapp <info@codelapp.com>
2018-05-03 09:39:08 +0200

    Commit final

--- EnviarRedesSociales/ViewController.swift
+++ EnviarRedesSociales/ViewController.swift
@@ -1,25 +1,38 @@
-//
-//  ViewController.swift
-//  EnviarRedesSociales
-//
-//  Created by jesus on 3/5/18.
-//  Copyright © 2018 jesus. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var texto: UITextField!
+    @IBOutlet weak var imagen: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(abrirGaleria))
+        imagen.addGestureRecognizer(tap)
+    }
+    
+    @objc func abrirGaleria(){
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let imagenTomada = info[UIImagePickerControllerEditedImage] as? UIImage
+        imagen.image = imagenTomada
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func compartir(_ sender: UIButton) {
+        let shared = UIActivityViewController(activityItems: [texto.text!, imagen.image!], applicationActivities: nil)
+        shared.popoverPresentationController?.sourceView = self.view
+        present(shared, animated: true, completion: nil)
     }
-
-
 }
 
